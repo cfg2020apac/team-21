22367829eaa4e2fcdacfaf147ed81418c5ec0468
Min Kyu Park <mkpark@connect.ust.hk>
2020-10-10 04:13:15 +0800

    updated logos
    updated logos on admin page and ngo page

--- react-client/src/main/admin/Admin.js
+++ react-client/src/main/admin/Admin.js
@@ -86,7 +86,7 @@ export default function Admin() {
                     <div className="messages-box" style={{ 'overflow': 'scroll', 'height': '90vh' }}>
                         <div className="list-group rounded-0">
                             <a className="list-group-item list-group-item-action active text-white rounded-0">
-                                <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width={50} className="rounded-circle" />
+                                <div className="media"><img src={require('../assets/images/Logo.png')} alt="user" width={50} className="rounded-circle" />
                                     <div className="media-body ml-4">
                                         <div className="d-flex align-items-center justify-content-between mb-1">
                                             <h6 className="mb-0">Hi! There</h6><small className="small font-weight-bold">25 Dec</small>
@@ -108,7 +108,7 @@ export default function Admin() {
 
                             {events.map(e =>
                                 <a key={e.id} href="#" className="list-group-item list-group-item-action list-group-item-light rounded-0" style={e.status == 'approved' ? { backgroundColor: '#007bff', color: 'white' } : {}} onClick={() => setSelectedEvent(e)}>
-                                    <div className="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width={50} className="rounded-circle" />
+                                    <div className="media"><img src={require('../assets/images/'+e.ngo_name+'.png')} alt="user" width={50} className="rounded-circle" />
                                         <div className="media-body ml-4">
                                             <div className="d-flex align-items-center justify-content-between mb-1">
                                                 <h6 className="mb-0">Sample Event - {e.status}</h6><small className="small font-weight-bold">{e.start_date}</small>
@@ -152,7 +152,7 @@ export default function Admin() {
                         </div>
                     </div>
                     {/* Sender Message*/}
-                    <div className="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width={50} className="rounded-circle" />
+                    <div className="media w-50 mb-3"><img src={require('../assets/images/'+selectedEvent.ngo_name+'.png')} alt="user" width={50} className="rounded-circle" />
                         <div className="media-body ml-3">
                             <div className="bg-light rounded py-2 px-3 mb-2">
                                 <p className="text-small mb-0 text-muted">The above is event detail! you wanna approve?</p>
