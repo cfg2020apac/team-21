e524136dd2d80c354b3544cf6c8360200c33a5e3
reval59 <modesty723@gmail.com>
2020-10-10 04:00:34 +0800

    integrate with dashboard

--- react-client/src/main/dashboard/Dashboard.js
+++ react-client/src/main/dashboard/Dashboard.js
@@ -33,21 +33,21 @@ const barData = {
         { a: 'Environmental education', b: 14 },
         { a: 'Hungers and homeless', b: 14 },
         { a: 'Children and youth', b: 13 },
-        {a:'People with disabilities; People with mental illness', b: 12},
-        {a: 'Children and youth; Environmental education', b:11},
-        {a:'Integrated', b:7},
-        {a:'Elderly; People with illness', b:6},
-        {a:'Children and youth; Ethnic minorities',b:4},
-        {a:'Children and youth; Ethnic minorities', b:2},
-        {a:'Women', b:2},
-        {a:'Environmental education; Integrated', b:2},
-        {a:'Adults; Children and youth; Elderly', b:2},
-        {a:'Environmental education; Women', b:1},
-        {a:'Adults; Elderly; Environmental education',b:1},
-        {a:'Children and youth; Refugees and asylum seekers', b:1},
-        {a:'Refugees and asylum seekers', b:1},
-        {a:'Adults; Children and youth; Elderly; People with disabilities', b:1},
-        {a:'People with mental illness', b:1}
+        { a: 'People with disabilities; People with mental illness', b: 12 },
+        { a: 'Children and youth; Environmental education', b: 11 },
+        { a: 'Integrated', b: 7 },
+        { a: 'Elderly; People with illness', b: 6 },
+        { a: 'Children and youth; Ethnic minorities', b: 4 },
+        { a: 'Children and youth; Ethnic minorities', b: 2 },
+        { a: 'Women', b: 2 },
+        { a: 'Environmental education; Integrated', b: 2 },
+        { a: 'Adults; Children and youth; Elderly', b: 2 },
+        { a: 'Environmental education; Women', b: 1 },
+        { a: 'Adults; Elderly; Environmental education', b: 1 },
+        { a: 'Children and youth; Refugees and asylum seekers', b: 1 },
+        { a: 'Refugees and asylum seekers', b: 1 },
+        { a: 'Adults; Children and youth; Elderly; People with disabilities', b: 1 },
+        { a: 'People with mental illness', b: 1 }
 
     ],
 }
@@ -63,14 +63,14 @@ const barData1 = {
         { a: 'Sha Tin', b: 56 },
         { a: 'Sham Shui Po', b: 118 },
         { a: 'Kwai Tsing', b: 86 },
-        {a:'Sai Kung', b: 72},
-        {a: 'Wong Tai Sin', b:97},
-        {a:'Yau Tsim Mong', b:88},
-        {a:'Tsuen Mun', b:72},
-        {a:'Southern' ,b:91},
-        {a:'Kwun Tong', b:57},
-        {a:'Wan Chai', b:31},
-        {a:'Eastern', b:60}
+        { a: 'Sai Kung', b: 72 },
+        { a: 'Wong Tai Sin', b: 97 },
+        { a: 'Yau Tsim Mong', b: 88 },
+        { a: 'Tsuen Mun', b: 72 },
+        { a: 'Southern', b: 91 },
+        { a: 'Kwun Tong', b: 57 },
+        { a: 'Wan Chai', b: 31 },
+        { a: 'Eastern', b: 60 }
 
     ],
 }
@@ -91,7 +91,7 @@ export default function Dashboard() {
                 <div class="col-sm-12">
                     <h2><VegaLite spec={spec} data={barData} /></h2>
                     <h2><VegaLite spec={spec} data={barData1} /></h2>
-                    <h2><Vegalite pie_spec={pie_spec} data={pieData} /></h2>
+                    <h2><VegaLite pie_spec={pie_spec} data={pieData} /></h2>
                 </div>
             </div>
         </div>
