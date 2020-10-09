f55d94d2453b99de3d4a6e217f26e628c827a9ac
Min Kyu Park <mkpark@connect.ust.hk>
2020-10-09 21:32:05 +0800

    Login error
    check login error

--- react-client/src/login/Login.js
+++ react-client/src/login/Login.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import 'login.css'
 
 export default class Login extends React.Component {
     render() {
-        return (
+        return (        
             <div>
-                Login
+                <div className="sidenav">
+                    <div className="login-main-text">
+                        <h2><br /> Login</h2>
+                        <p>Login or register.</p>
+                    </div>
+                </div>
+                <div className="main">
+                    <div className="col-md-6 col-sm-12">
+                        <div className="login-form">
+                            <form>
+                                <div className="form-group">
+                                    <label>User Name</label>
+                                    <input type="text" className="form-control" placeholder="User Name" />
+                                </div>
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input type="password" className="form-control" placeholder="Password" />
+                                </div>
+                                <button type="submit" className="btn btn-black">Login</button>
+                                <button type="submit" className="btn btn-secondary pink">Register</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
